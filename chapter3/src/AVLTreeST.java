afd48ed11c86e3e1cbdadd61b6a19875db282f54
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-10 13:32:29 -0700

    added AVLtree class

---
+++ chapter3/src/AVLTreeST.java
@@ -0,0 +1,15 @@
+public class AVLTreeST<Key extends Comparable<Key>, Value> {
+
+    private class Node{
+        Node left;
+        Node right;
+        Key key;
+        Value value;
+        int size;
+        int height;
+
+        public Node(Key key, Value val, int size, int height){
+
+        }
+    }
+}
