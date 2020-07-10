ae55c3e5b00b2109a353b1be619e0925b77cfa3d
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-10 13:34:29 -0700

    added Node class

--- chapter3/src/AVLTreeST.java
+++ chapter3/src/AVLTreeST.java
@@ -1,15 +1,20 @@
 public class AVLTreeST<Key extends Comparable<Key>, Value> {
 
+    private Node root;
+
     private class Node{
-        Node left;
-        Node right;
-        Key key;
-        Value value;
-        int size;
-        int height;
+        private Node left;
+        private Node right;
+        private final Key key;
+        private Value val;
+        private int size;
+        private int height;
 
         public Node(Key key, Value val, int size, int height){
-
+            this.key = key;
+            this.val = val;
+            this.size = size;
+            this.height = height;
         }
     }
 }
