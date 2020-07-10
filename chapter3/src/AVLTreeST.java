35ab635823edbb2b31fcb5c5c8b54d62fac91f2d
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-10 13:42:55 -0700

    added methods

--- chapter3/src/AVLTreeST.java
+++ chapter3/src/AVLTreeST.java
@@ -17,4 +17,46 @@ public class AVLTreeST<Key extends Comparable<Key>, Value> {
             this.height = height;
         }
     }
+
+    public Value get(Key key){
+        return get(root, key).val;
+    }
+
+    private Node get(Node x, Key key){
+        if(x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if(cmp < 0) return get(x.left, key);
+        else if(cmp > 0) return get(x.right, key);
+        else return x;
+    }
+
+    private Node insert(Node x, Key key, Value val){
+        if(x == null) return new Node(key,val, 1, -1);
+        int cmp = key.compareTo(x.key);
+        if(cmp < 0) x.left = insert(x.left, key, val);
+        else if(cmp > 0) x.right = insert(x.right, key, val);
+        else x.val = val;
+        // update values of height and size
+        x.size = size(x.left) + size(x.right) + 1;
+        x.height = 1 + Integer.max(height(x.left), height(x.right));
+        return x;
+    }
+
+    public int height(){
+        return height(root);
+    }
+
+    private int height(Node x){
+        if(x == null) return 0;
+        else return x.height;
+    }
+
+    public int size(){
+        return size(root);
+    }
+
+    private int size(Node x){
+        if(x == null) return 0;
+        else return x.size;
+    }
 }
