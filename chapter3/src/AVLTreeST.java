188e9bfd023e76f76eef3e2dc0a4fb438cdcddb2
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-10 13:58:07 -0700

    added delete method

--- chapter3/src/AVLTreeST.java
+++ chapter3/src/AVLTreeST.java
@@ -41,6 +41,28 @@ public class AVLTreeST<Key extends Comparable<Key>, Value> {
         return x;
     }
 
+    public void delete(Key key){
+        root = delete(root, key);
+    }
+
+    private Node delete(Node x, Key key){
+        if(x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if(cmp < 0) x.left = delete(x.left, key);
+        else if(cmp > 0) x.right = delete(x.right, key);
+        else {
+            if(x.left == null) return x.right;
+            if(x.right == null) return x.left;
+            Node t = x;
+            x = min(t.right);
+            x.right = deleteMin(t.right);
+            x.left = t.left;
+        }
+        x.size = size(x.left) + size(x.right) + 1;
+        x.height = 1 + Integer.max(height(x.left), height(x.right));
+        return x;
+    }
+
     public int height(){
         return height(root);
     }
