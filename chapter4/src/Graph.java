c342d1457ba92f4585ce212821931ba3b07bb18c
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-15 12:56:52 -0700

    added constructor

--- chapter4/src/Graph.java
+++ chapter4/src/Graph.java
@@ -15,12 +15,23 @@ public class Graph {
         }
     }
 
+    public Graph(In in){
+        this(in.readInt());
+        int E = in.readInt();
+        for(int i = 0;i < E; ++i){
+            int v = in.readInt();
+            int w = in.readInt();
+            addEdge(v, w);
+        }
+    }
+
     public int V(){return V;}
     public int E(){return E;}
 
     public void addEdge(int v, int w){
         adj[v].add(w);
         adj[w].add(v);
+        E++;
     }
 
     public Iterable<Integer> adj(int v){
