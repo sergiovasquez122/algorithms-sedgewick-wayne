1d4f28fd39beae9238658ab6c149b4c77f3bc874
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-16 13:54:53 -0700

    added Graph helpers

--- chapter4/src/Graph.java
+++ chapter4/src/Graph.java
@@ -8,6 +8,32 @@ public class Graph {
     private int E;
     private Bag<Integer>[] adj;
 
+    public static int degree(Graph G, int v){
+        int degree = 0;
+        for(int w : G.adj(v)) degree++;
+        return degree;
+    }
+
+    public static int maxDegree(Graph G){
+        int max = 0;
+        for(int i = 0;i < G.V(); i++){
+            max = Integer.max(max, degree(G, i));
+        }
+        return max;
+    }
+
+    public static int avgDegree(Graph G){
+        return 2 * G.E / G.V();
+    }
+
+    public static int numberOfSelfLoops(Graph G){
+        int count = 0;
+        for(int v = 0;v < G.V(); ++v)
+            for(int w : G.adj(v))
+                if(v == w) count++;
+        return count / 2;
+    }
+
     public Graph(int V){
         this.V = V;
         this.E = 0;
