5767c6c64341977da98bec93290f63745478d1f7
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-17 17:55:06 -0700

    added partial implementation

--- chapter4/src/OnePassSymbolGraph.java
+++ chapter4/src/OnePassSymbolGraph.java
@@ -1,2 +1,51 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.SET;
+import edu.princeton.cs.algs4.ST;
+
 public class OnePassSymbolGraph {
+    private ST<String, SET<String>> st;
+
+    public OnePassSymbolGraph(){
+        st = new ST<>();
+    }
+    public OnePassSymbolGraph(String filename, String delimiter){
+        st = new ST<>();
+        In in = new In(filename);
+        while(in.hasNextLine()){
+            String[] names = in.readLine().split(delimiter);
+            String v = names[0];
+            for(int i = 1;i < names.length;++i)
+                addEdge(v, names[i]);
+        }
+    }
+
+    public boolean contains(String name){
+        return st.contains(name);
+    }
+
+    public Iterable<String> vertices(){
+        return st.keys();
+    }
+
+    public Iterable<String> adjTo(String v){
+        return st.get(v);
+    }
+
+    public void addEdge(String u, String v){
+        if(!st.contains(u)) st.put(u, new SET<>());
+        if(!st.contains(v)) st.put(v, new SET<>());
+        st.get(u).add(v);
+        st.get(v).add(u);
+    }
+
+    public String toString(){
+        String s = "";
+        for(String v : vertices()){
+            s += v + " ";
+                for(String w : adjTo(v))
+                    s += w + " ";
+            s += "\n";
+        }
+        return s;
+    }
 }
