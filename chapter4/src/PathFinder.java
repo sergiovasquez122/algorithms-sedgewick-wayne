d3ad3e1d173efea88a3888a2becaf2707b8501b2
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-17 18:19:31 -0700

    added pathfinder

---
+++ chapter4/src/PathFinder.java
@@ -0,0 +1,41 @@
+import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.ST;
+import edu.princeton.cs.algs4.Stack;
+
+public class PathFinder {
+
+    private ST<String, Integer> dist;
+    private ST<String, String> parent;
+
+    public PathFinder(OnePassSymbolGraph G, String s){
+        parent = new ST<>();
+        dist = new ST<>();
+        Queue<String> queue = new Queue<>();
+        queue.enqueue(s);
+        dist.put(s, 0);
+        while(!queue.isEmpty()){
+            String curr = queue.dequeue();
+
+            for(String w : G.adjTo(curr)){
+                if(!parent.contains(w)){
+                    parent.put(w, curr);
+                    dist.put(w, dist.get(curr) + 1);
+                    queue.enqueue(w);
+                }
+            }
+        }
+    }
+
+    public int distanceTo(String v){
+        return dist.get(v);
+    }
+
+    public Iterable<String> pathTo(String v){
+        Stack<String> path = new Stack<>();
+        while(v != null & dist.contains(v)){
+            path.push(v);
+            v = parent.get(v);
+        }
+        return path;
+    }
+}
