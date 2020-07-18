526777301196214564cb247e901e19bad218015a
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-17 19:08:00 -0700

    added partial implementation of imageprocessing

--- chapter4/src/ImageProcessing.java
+++ chapter4/src/ImageProcessing.java
@@ -1,2 +1,35 @@
+import edu.princeton.cs.algs4.StdOut;
+
 public class ImageProcessing {
+
+    // note this implementation assumes a image of 0's and 1's
+    public ImageProcessing(boolean[][] image, int x, int y){
+        boolean sourceColor = image[x][y];
+        dfs(image, x, y, sourceColor);
+    }
+
+    private void dfs(boolean[][] image, int x, int y, boolean color){
+        if(x < 0 || x >= image.length || y < 0 || y >= image[0].length || image[x][y] != color){
+            return;
+        }
+        image[x][y] = !color;
+        int[][] positions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        for(int[] pos : positions){
+            dfs(image, x + pos[0], y + pos[1], color);
+        }
+    }
+
+    public static void main(String[] args) {
+        boolean [][] image = {{false, false, false, false},
+                {false, false, false, false},
+                {false, false, false, false},
+                {false, false, false, false}};
+        ImageProcessing imageProcessing = new ImageProcessing(image, 0, 0);
+        for(int i = 0;i < image.length;++i){
+            for(int j = 0;j < image.length;++j){
+                StdOut.print(image[i][j] + " ");
+            }
+            StdOut.println();
+        }
+    }
 }
