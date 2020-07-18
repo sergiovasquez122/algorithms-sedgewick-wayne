618d0e5ab53a8272dc6e2f36c9eb5e29dffca21d
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-18 16:04:18 -0700

    added UFSearch class

--- chapter4/src/UFSearch.java
+++ chapter4/src/UFSearch.java
@@ -1,2 +1,19 @@
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 public class UFSearch {
+
+    private WeightedQuickUnionUF UF;
+    public UFSearch(Graph G, int s){
+        UF = new WeightedQuickUnionUF(G.V());
+    }
+
+    boolean marked(int v){
+        return false;
+    }
+
+    int count(){
+        return 0;
+    }
+
+
 }
