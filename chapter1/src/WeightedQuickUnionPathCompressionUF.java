7570025d44664f76bc28d196485e8b670f3cc50b
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-20 17:47:56 -0700

    completed WeightedQuickUnionFindPathCompressionUF

--- chapter1/src/WeightedQuickUnionPathCompressionUF.java
+++ chapter1/src/WeightedQuickUnionPathCompressionUF.java
@@ -1,2 +1,40 @@
 public class WeightedQuickUnionPathCompressionUF {
+    private int[] id;
+    private int[] sz;
+    private int count;
+
+    public WeightedQuickUnionPathCompressionUF(int N){
+        count = N;
+        id = new int[N];
+        for(int i = 0;i < N; i++) id[i] = i;
+        sz = new int[N];
+        for(int i = 0;i < N; i++) sz[i] = 1;
+    }
+
+    public int count(){
+        return count;
+    }
+
+    private int find(int p){
+        int root = p;
+        while(root != id[root])
+            root = id[root];
+        while(p != root){
+            int newP = id[p];
+            id[p] = root;
+            p = newP;
+        }
+        return root;
+    }
+
+    public void union(int p, int q){
+        int i = find(p);
+        int j = find(q);
+        if(i == j) return;
+
+        if(sz[i] < sz[j]){id[i] = j;sz[j] += sz[i];}
+        else {id[j] = i;sz[i] += sz[j];}
+        count--;
+    }
 }
+
