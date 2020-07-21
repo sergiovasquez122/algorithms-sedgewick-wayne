61dce86feca4956ba120af06a7aab2587cf7ebef
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-21 11:25:04 -0700

    added implementation of randomGrid

--- chapter1/src/RandomGrid.java
+++ chapter1/src/RandomGrid.java
@@ -1,5 +1,7 @@
+import edu.princeton.cs.algs4.StdOut;
+
 public class RandomGrid {
-    private class Connection {
+    private static class Connection {
         int p;
         int q;
         public Connection(int p, int q){
@@ -8,12 +10,22 @@ public class RandomGrid {
         }
     }
 
-    public void generate(int N){
-        
+    public static RandomizedQueue<Connection> generate(int N){
+        RandomizedQueue<Connection> queue = new RandomizedQueue<>();
+        for(int i = 0;i < N;++i){
+            for(int j = i + 1;j < N;++j){
+                queue.enqueue(new Connection(i, j));
+                queue.enqueue(new Connection(j, i));
+            }
+        }
+        return queue;
     }
 
     public static void main(String[] args) {
         int N = Integer.parseInt(args[0]);
-
+        RandomizedQueue<Connection> Q = generate(N);
+        for(Connection c : Q){
+            StdOut.println(c.p + " " + c.q);
+        }
     }
 }
