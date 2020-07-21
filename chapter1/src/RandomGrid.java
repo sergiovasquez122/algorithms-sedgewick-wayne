3c491893a7debe27a727a464c85894ca77918c25
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-21 11:18:07 -0700

    added RandomGrid

---
+++ chapter1/src/RandomGrid.java
@@ -0,0 +1,12 @@
+public class RandomGrid {
+    private class Connection {
+        int p;
+        int q;
+        public Connection(int p, int q){
+            this.p = p;
+            this.q = q;
+        }
+    }
+
+
+}
