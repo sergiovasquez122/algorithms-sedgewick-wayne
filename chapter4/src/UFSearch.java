c67cd6ab2cbf77a1c40361e89982fe9d3fe83199
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-24 15:29:30 -0700

    partial implementation of UFSearch

--- chapter4/src/UFSearch.java
+++ chapter4/src/UFSearch.java
@@ -1,3 +1,4 @@
+import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class UFSearch {
@@ -23,5 +24,19 @@ public class UFSearch {
         return 0;
     }
 
+    public static void main(String[] args) {
+        Graph G = new Graph(5);
+        G.addEdge(0, 1);
+        G.addEdge(0, 2);
+        G.addEdge(0, 3);
+        G.addEdge(1, 2);
+        G.addEdge(2,3);
 
+        UFSearch ufSearch = new UFSearch(G, 1);
+        StdOut.println("Connected to 0: " + ufSearch.marked(0) + " expected: true");
+        StdOut.println("Connected to 1: " + ufSearch.marked(1) + " expected: true");
+        StdOut.println("Connected to 2: " + ufSearch.marked(1) + " expected: true");
+        StdOut.println("Connected to 3: " + ufSearch.marked(3) + " expected: true");
+        StdOut.println("Connected to 5: " + ufSearch.marked(4) + " expected: false");
+    }
 }
