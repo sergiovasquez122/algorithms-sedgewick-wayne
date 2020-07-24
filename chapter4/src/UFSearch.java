374fb177cd9abbd217af9ab7a919cc3ef8413ac1
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-24 15:20:01 -0700

    partial implementation of UFSearch

--- chapter4/src/UFSearch.java
+++ chapter4/src/UFSearch.java
@@ -3,12 +3,20 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 public class UFSearch {
 
     private WeightedQuickUnionUF UF;
-    public UFSearch(Graph G, int s){
+    private int s;
+    public UFSearch(Graph G, int s)
+    {
         UF = new WeightedQuickUnionUF(G.V());
+        this.s = s;
+        for(int i = 0;i < G.V();++i){
+            for(int w : G.adj(i)){
+                UF.union(i, w);
+            }
+        }
     }
 
     boolean marked(int v){
-        return false;
+        return UF.connected(s,v);
     }
 
     int count(){
