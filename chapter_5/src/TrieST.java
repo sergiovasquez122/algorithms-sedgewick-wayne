4d6eae5d422b5e8f7f12f6181b76134bbfe2a204
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-27 13:33:52 -0700

    added iterable methods

--- chapter_5/src/TrieST.java
+++ chapter_5/src/TrieST.java
@@ -15,6 +15,16 @@ public class TrieST<Value> {
         return (Value) x.val;
     }
 
+    public Iterable<String> keys(){
+        return keysWithPrefix("");
+    }
+
+    public Iterable<String> keysWithPrefix(String pre){
+        Queue<String> q = new Queue<>();
+        collect(get(root, pre, 0), pre, q);
+        return q;
+    }
+
     private void collect(Node x, String pre, Queue<String> q){
         if(x == null) return;
         if(x.val != null) q.enqueue(pre);
