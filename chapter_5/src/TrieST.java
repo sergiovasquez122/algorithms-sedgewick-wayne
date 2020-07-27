8d8c7abe1d17c485eb695f32a2222dc081e3616f
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-27 13:28:42 -0700

    added TrieST

--- chapter_5/src/TrieST.java
+++ chapter_5/src/TrieST.java
@@ -19,4 +19,16 @@ public class TrieST<Value> {
         char c = key.charAt(d);
         return get(x.next[c], key, d + 1);
     }
+
+    public void put(String key, Value val){
+        root = put(root, key, val, 0);
+    }
+
+    private Node put(Node x, String key,Value val, int d){
+        if(x == null) x = new Node();
+        if(d == key.length()){x.val = val; return x;}
+        char c = key.charAt(d);
+        x.next[c] = put(x.next[c], key, val, d + 1);
+        return x;
+    }
 }
