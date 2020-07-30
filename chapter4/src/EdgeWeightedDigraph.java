a426e04bcfa4cba06c882cecebd527cdd454721d
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-30 16:28:57 -0700

    added DijkstraSP

--- chapter4/src/EdgeWeightedDigraph.java
+++ chapter4/src/EdgeWeightedDigraph.java
@@ -1,4 +1,41 @@
-package PACKAGE_NAME;
+import edu.princeton.cs.algs4.Bag;
+import edu.princeton.cs.algs4.Edge;
 
 public class EdgeWeightedDigraph {
+    private final int V;
+    private int E;
+    private Bag<DirectedEdge>[] adj;
+
+    public EdgeWeightedDigraph(int V){
+        this.V = V;
+        this.E = 0;
+        adj = (Bag<DirectedEdge>[]) new Bag[V];
+        for(int v = 0;v < V;v++)
+            adj[v] = new Bag<>();
+    }
+
+    public int V(){
+        return V;
+    }
+
+    public int E(){
+        return E;
+    }
+
+    public void addEdge(DirectedEdge e){
+        adj[e.from()].add(e);
+        E++;
+    }
+
+    public Iterable<DirectedEdge> adj(int v){
+        return adj[v];
+    }
+
+    public Iterable<DirectedEdge> edges(){
+        Bag<DirectedEdge> bag = new Bag<>();
+        for(int v = 0;v < V; v++)
+            for(DirectedEdge e : adj[v])
+                bag.add(e);
+        return bag;
+    }
 }
